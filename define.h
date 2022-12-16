d55bb24f78dacd58f231d57ec8dce7544b2d3426
Kudl1k <stepanek.kudlacek@gmail.com>
2022-12-16 21:17:01 +0100

    add colors

--- define.h
+++ define.h
@@ -26,8 +26,10 @@ typedef struct
     int x;
     int y;
     int shape[4][4];
-    int size;
+    int width;
+    int height;
     int orientation;    //* 0 - start, 1 - prvni cyklus, 2 - druhy cyklus, 3 - treti cyklus
+    SDL_Color color;
 }Tetrino;
 
 Tetrino block[7] ={{
@@ -36,41 +38,48 @@ Tetrino block[7] ={{
     0,0,0,0,            //! Tvar I
     0,0,0,0,
     0,0,0,0,
-    4,0},{
+    4,1,0,
+    0,255,255},{
     START_X,START_Y,
     1,1,0,0,
     1,1,0,0,            //! Tvar O
     0,0,0,0,
     0,0,0,0,
-    2,0},{
+    2,2,0,
+    255,255,0},{
     START_X,START_Y,
     0,1,0,0,
     1,1,1,0,            //! Tvar T
     0,0,0,0,
     0,0,0,0,
-    3,0},{
+    3,2,0,
+    128,0,128},{
     START_X,START_Y,
     0,1,0,0,
     0,1,0,0,            //! Tvar J
     1,1,0,0,
     0,0,0,0,
-    3,0},{
+    3,2,0,
+    0,0,255},{
     START_X,START_Y,
     1,0,0,0,
     1,0,0,0,            //! Tvar L
     1,1,0,0,
     0,0,0,0,
-    3,0},{
+    3,2,0,
+    255,165,0},{
     START_X,START_Y,
     0,1,1,0,
     1,1,0,0,            //! Tvar S
     0,0,0,0,
     0,0,0,0,
-    3,0},{
+    3,2,0,
+    78,252,78},{
     START_X,START_Y,
     1,1,0,0,
     0,1,1,0,            //! Tvar Z
     0,0,0,0,
     0,0,0,0,
-    3,0}
+    3,2,0,
+    255,0,0}
     },cur;
